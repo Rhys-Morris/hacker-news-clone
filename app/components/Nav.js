0525c040b7ce92beb3db6b7cea487accc7826fb3
Rhys Morris <rhysmorris08@gmail.com>
2021-05-04 16:55:55 +1000

    Added theme context and consumer to toggle global theme on navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { ThemeConsumer } from "../contexts/theme";
 
 const activeStyle = {
   color: "rgb(189, 54, 39)",
@@ -8,17 +9,32 @@ const activeStyle = {
 export default class Nav extends React.Component {
   render() {
     return (
-      <nav className="nav">
-        <div className="nav__link-box">
-          <NavLink to="/" className="nav__link" exact activeStyle={activeStyle}>
-            Top
-          </NavLink>
-          <NavLink to="/new" className="nav__link" activeStyle={activeStyle}>
-            New
-          </NavLink>
-        </div>
-        <p>ICON</p>
-      </nav>
+      <ThemeConsumer>
+        {({ theme, toggleTheme }) => (
+          <nav className="nav">
+            <div className="nav__link-box">
+              <NavLink
+                to="/"
+                className="nav__link"
+                exact
+                activeStyle={activeStyle}
+              >
+                Top
+              </NavLink>
+              <NavLink
+                to="/new"
+                className="nav__link"
+                activeStyle={activeStyle}
+              >
+                New
+              </NavLink>
+            </div>
+            <div className="nav__theme" onClick={toggleTheme}>
+              {theme === "light" ? "🌕" : "🌞"}
+            </div>
+          </nav>
+        )}
+      </ThemeConsumer>
     );
   }
 }
--- app/index.js
+++ app/index.js
@@ -4,15 +4,30 @@ import "./index.css";
 import ArticleList from "./components/ArticleList.js";
 import Nav from "./components/Nav.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/theme.js";
 
-const App = () => (
-  <Router>
-    <div className="container">
-      <Nav />
-      <Route exact path="/" component={ArticleList} />
-      <Route path="/new" component={ArticleList} />
-    </div>
-  </Router>
-);
+class App extends React.Component {
+  state = {
+    theme: "light",
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === "light" ? "dark" : "light",
+      }));
+    },
+  };
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className="container">
+            <Nav />
+            <Route exact path="/" component={ArticleList} />
+            <Route path="/new" component={ArticleList} />
+          </div>
+        </ThemeProvider>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
