32034a27cde59592076d15510ae3f70f7d7094a5
Rhys Morris <rhysmorris08@gmail.com>
2021-05-05 18:29:25 +1000

    Implemented code splitting with React.Lazy and React.Suspense for User and Post components

--- app/index.js
+++ app/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import ArticleList from "./components/ArticleList.js";
 import Nav from "./components/Nav.js";
-import User from "./components/User.js";
-import Post from "./components/Post.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "./contexts/theme.js";
+import Loading from "./components/Loading";
+
+const User = React.lazy(() => import("./components/User.js"));
+const Post = React.lazy(() => import("./components/Post.js"));
 
 class App extends React.Component {
   state = {
@@ -23,19 +25,21 @@ class App extends React.Component {
         <ThemeProvider value={this.state}>
           <div className={`container ${this.state.theme}`}>
             <Nav />
-            <Switch>
-              <Route exact path="/" component={ArticleList} key={1} />
-              <Route exact path="/new" component={ArticleList} key={2} />
-              <Route exact path="/user" component={User} />
-              <Route exact path="/post" component={Post} />
-              <Route
-                render={() => (
-                  <div className={`no-content ${this.state.theme}`}>
-                    Content Not Found
-                  </div>
-                )}
-              />
-            </Switch>
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" component={ArticleList} key={1} />
+                <Route exact path="/new" component={ArticleList} key={2} />
+                <Route exact path="/user" component={User} />
+                <Route exact path="/post" component={Post} />
+                <Route
+                  render={() => (
+                    <div className={`no-content ${this.state.theme}`}>
+                      Content Not Found
+                    </div>
+                  )}
+                />
+              </Switch>
+            </React.Suspense>
           </div>
         </ThemeProvider>
       </Router>
